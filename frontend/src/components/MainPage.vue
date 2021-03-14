b6172dd3aa815efc31296d4e7784e4c490739497
griffinGC <griffindouble@gmail.com>
2021-03-14 22:54:04 +0900

    init mainpage

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,30 +1,54 @@
 <template>
   <div class="MainPage">
-    <h1>{{ "Welcome to Your DS Solutions!" }}</h1>
-    <p>
+    <!-- <h1>{{ "Welcome to Your DS Solutions!" }}</h1> -->
+    <!-- <p>
       {{"여기는 메인 페이지에요!"}}
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </p> -->
+    <img id="main-banner" src="../assets/mainbanner.png" alt="main-banner">
+    <!-- 반복문으로 처리 -->
+    <h2>{{"News"}}</h2>
+    <div id="news-list">
+      <div id="news">
+        <img id="news-img" src="../assets/apache.png" alt="news-img">
+        <div id="news-data">
+          <h2 id="news-title">
+            뉴스 타이틀
+          </h2>
+          <p id="news-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit vitae delectus deserunt</p>
+        </div>
+      </div>
+      <div id="news">
+        <img id="news-img" src="../assets/apache.png" alt="news-img">
+        <div id="news-data">
+          <h2 id="news-title">
+            뉴스 타이틀
+          </h2>
+          <p id="news-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit vitae delectus deserunt</p>
+        </div>
+      </div>
+      <div id="news">
+        <img id="news-img" src="../assets/apache.png" alt="news-img">
+        <div id="news-data">
+          <h2 id="news-title">
+            뉴스 타이틀
+          </h2>
+          <p id="news-contents">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit vitae delectus deserunt</p>
+        </div>
+      </div>
+    </div>
+
+    <div id="cooporate">
+      <h2>Cooporate</h2>
+      <div id="cooporate-list">
+        <img id="cooporate-img" src="../assets/apache.png" alt="news-img">
+        <img id="cooporate-img" src="../assets/apache.png" alt="news-img">
+        <img id="cooporate-img" src="../assets/apache.png" alt="news-img">
+        <img id="cooporate-img" src="../assets/apache.png" alt="news-img">
+        <img id="cooporate-img" src="../assets/apache.png" alt="news-img">
+      </div>
+
+    </div>
+    
   </div>
 </template>
 
@@ -38,8 +62,56 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#main-banner{
+  width: 100%;
+  height: 150%;
+}
+#news-img{
+  width: 100%;
+  height: 100%;
+}
+#news-list{
+  display: flex;
+  width: 100%;
+  height: 140px;
+}
+#news{
+  display: flex;
+  padding-left: 5%;
+  padding-right: 5%;
+}
+#news-title{
+  max-height: 50%;
+}
+#news-data{
+  padding-left: 5%;
+  padding-right: 5%
+}
+#news-contents{
+  width: 100%;
+  height: 50%;
+  padding-left: 5%;
+  padding-right: 5%;
+  text-align: left;
+  overflow: hidden;
+}
+#cooporate{
+  padding-top: 5%;
+  height: 20%;
+}
+#cooporate-list{
+  display: flex;
+  justify-content: space-between;
+  max-height: inherit;
+  max-width: 100%;
+  padding-left: 5%;
+  padding-right: 5%;
+}
+#cooporate-img{
+  padding-left: 1%;
+  padding-right: 1%;
+  max-width: 10%;
+  height: auto;
 }
 ul {
   list-style-type: none;
