554a549da9ea55fb01b7f7e79b02a37ad2fe9c27
SangOk <sangok1993@naver.com>
2021-03-21 16:59:40 +0900

    company

--- frontend/src/components/Company.vue
+++ frontend/src/components/Company.vue
@@ -2,15 +2,16 @@
     <div class="Company">
         <div id="introduce">
             <img id="rogo" src="../assets/대충만든로고.png" alt="main-banner">
-            <h1> DS Solution </h1>
-            <br>
-            <div id="detail">
+
+            <div id="detail" style="background-color:white">
+                <h1> DS Solution </h1><br>
                 DS solution은 2014년 설립되었으며 여러 기업에 지원을 하면서 안정적인 사업을 제공하고 있습니다. <br>
                 회사소개 디테일. <br>
                 <br>
             </div>
+            <br>
         </div>
-        <br><div style="width:100%; height:4px; background-color:black;"/><br>
+        <div style="width:100%; height:4px; background-color:black;"/><br>
         <table class="history">
             <th id="photo">
                 <img id="history_photo" src="../assets/연혁.png">
@@ -30,9 +31,10 @@
             </th>
         </table>
         
-        <br><div style="width:100%; height:4px; background-color:black;"/><br>
+        <br><div style="width:100%; height:4px; background-color:black;"/>
 
-        <div id="industry">
+        <div id="industry" style="background-color:white">
+            <br>
             <h1>주요사업 소개</h1>
             <div id="industry-list">
                 <img id="industry-img" src="../assets/apache.png" alt="news-img">
@@ -40,9 +42,10 @@
                 <img id="industry-img" src="../assets/apache.png" alt="news-img">
                 <img id="industry-img" src="../assets/apache.png" alt="news-img">
             </div>
+            <br>
         </div>
 
-        <br><div style="width:100%; height:4px; background-color:black;"/><br>
+        <div style="width:100%; height:4px; background-color:black;"/><br>
 
         <div id="certificate">
             <h1>DSSolution Certificate</h1>
@@ -71,6 +74,9 @@ export default {
 }
 .history{
     width:100%;
+}
+#introduce{
+    background-image: url(../assets/로봇팔.png);
 }
 #photo{
     float:left;
