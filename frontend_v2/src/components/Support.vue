d80e6202a2631cc04d0f5c011626a9dbf6a98bb7
SangOk <sangok1993@naver.com>
2021-03-29 04:40:53 +0900

    update

--- frontend_v2/src/components/Support.vue
+++ frontend_v2/src/components/Support.vue
@@ -1,6 +1,12 @@
 <template lang="">
     <div class="Support">
-        {{"고객지원 페이지여~~~"}}
+        <a id="title">
+            회사찾아오시는 길
+        </a>
+        <div id="location">
+            <img class="img" src="../assets/회사위치.png">
+            
+        </div>
     </div>
 </template>
 <script>
@@ -9,6 +15,23 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    .Support{
+        width: 100%;
+        display:table;
+    }
+    .title{
+        padding: 5px;
+        font-size: 60px;
+        margin:0 auto;
+    }
+    #location{
+        margin:0 auto;
+        width:1040px;
+    }
+    #location .img{
+        width:800px;
+        height:auto;
+        margin:0 auto;
+    }
 </style>
